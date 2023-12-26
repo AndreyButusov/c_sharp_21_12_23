2d2aec8c9f35e6d292731150e26b6a39852515e4
Andrey Butusov <agbutusov@mail.ru>
2023-12-26 22:53:13 +0300

    Lesson2: task4

---
+++ lesson_2/2_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// 4.  Напишите программу, которая выводит третью с конца
+//     цифру заданного числа или сообщает, что третьей цифры
+//     нет.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+if (num >= 100)
+    Console.WriteLine(num / 100 % 10);
+else
+    Console.WriteLine("Третьей цифры нет");
