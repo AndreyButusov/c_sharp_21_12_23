868b2d32fe153da29d1c6c5f39ade744ab7a8368
Andrey Butusov <agbutusov@mail.ru>
2023-12-27 23:08:50 +0300

    Add Homework: Task3

---
+++ lesson_2/Homework/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход целое число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа
+
+Console.WriteLine("Введите двузначное число из отрезка [10, 99]");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 10 || num > 99)
+    Console.WriteLine("Введено число вне указанного диапазона");
+else
+    {
+        if (num / 10 == num % 10)
+            Console.WriteLine("Первая и вторая цифра введенного числа равны");
+        else
+        {
+            if (num / 10 > num % 10)
+                Console.WriteLine(num / 10);
+            else
+                Console.WriteLine(num % 10);
+        }
+    }
