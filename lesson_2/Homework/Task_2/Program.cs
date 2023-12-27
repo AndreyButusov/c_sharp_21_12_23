f65b2a4a69098ce6df306c0aefdf44c14db940a6
Andrey Butusov <agbutusov@mail.ru>
2023-12-27 22:55:24 +0300

    Add Homework: Task2

---
+++ lesson_2/Homework/Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+// в которой находится эта точка.
+
+Console.WriteLine("Введите координату X точки");
+int X = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y точки");
+int Y = int.Parse(Console.ReadLine()!);
+
+if (X == 0 || Y == 0)
+    Console.WriteLine("Точка находится на координатной плоскости");
+else
+    {
+        if (X > 0 && Y > 0)
+            Console.WriteLine("Точка находится в I четверти");
+        if (X > 0 && Y < 0)
+            Console.WriteLine("Точка находится в II четверти");
+        if (X < 0 && Y < 0)
+            Console.WriteLine("Точка находится в III четверти");
+        if (X < 0 && Y > 0)
+            Console.WriteLine("Точка находится в IV четверти");
+    }
