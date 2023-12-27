c91bd4f7ccc58ca350bd76e81854e23dc5d7e7a7
Andrey Butusov <agbutusov@mail.ru>
2023-12-28 00:11:03 +0300

    Add Homework: Task4

---
+++ lesson_2/Homework/Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число");
+int N = int.Parse(Console.ReadLine()!);
+string str1 = "";
+while (N > 0)
+{
+    if (str1 == "")
+    {
+        str1 = str1 + N % 10;
+        N = N / 10;
+    }
+    else
+    {
+        str1 = N % 10 + ", " + str1;
+        N = N / 10;
+    }
+}
+Console.Write(str1);
