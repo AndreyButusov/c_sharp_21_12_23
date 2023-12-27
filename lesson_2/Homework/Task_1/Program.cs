cd403a88f84bd33a600043ea4989c96c28283a27
Andrey Butusov <agbutusov@mail.ru>
2023-12-27 22:30:55 +0300

    добавил первую задачу домашней работы

---
+++ lesson_2/Homework/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 7 == 0 && num % 23 == 0)
+    Console.WriteLine("Число " + num + " кратно 7 и 23");
+else
+    {
+        if (num % 7 != 0)
+            Console.WriteLine("Число " + num + " НЕ кратно 7");
+        if (num % 23 != 0)
+            Console.WriteLine("Число " + num + " НЕ кратно 23");
+    }
