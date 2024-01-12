c794f234c7c970d16cc0d869df0fec5fce34de53
Andrey Butusov <agbutusov@mail.ru>
2024-01-13 00:28:54 +0300

    Add task1 - Find number

--- lesson_3/3_1/Program.cs
+++ lesson_3/3_1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массиив. Напишите программу, которая определяет, 
+//  присутствует ли заданное число в массиве. Программа
+//  должна выдавать ответ: Да/нет.
+
+Console.WriteLine("Введите значение");
+int a = int.Parse(Console.ReadLine()!);
+int[] arr = { 1, 2, 3, 4, 5 };
+int i = 0;
+bool found = false;
+
+
+while (i < arr.Length)
+{
+    if (a == arr[i])
+    {
+        found = true;
+        break;
+    }
+    i++;
+}
+
+if (found)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
