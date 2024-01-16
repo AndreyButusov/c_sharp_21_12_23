fdd9aeeeae341491cc1a4d74f00a58393ef0d99d
Andrey Butusov <agbutusov@mail.ru>
2024-01-16 22:55:35 +0300

    добавил задачу №4, работает

---
+++ lesson_3/3_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int Num = int.Parse(Console.ReadLine()!);
+
+if (Num < 100 || Num > 999)
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
+else
+{
+int size = 3;
+int[] arr = new int[size];
+for (int i = 0; i < size; i++)
+{
+    arr[i] = Num % 10;
+    Num = Num / 10;
+    Console.Write($"{arr[i]} ");
+}
+}
