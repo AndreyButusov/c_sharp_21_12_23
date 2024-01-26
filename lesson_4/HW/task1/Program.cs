73e699a7b02c6ca8849a96d7fe7b9df504283dad
Andrey Butusov <agbutusov@mail.ru>
2024-01-26 23:14:00 +0300

    добавил 2ой вариант решения 1 задачи

--- lesson_4/HW/task1/Program.cs
+++ lesson_4/HW/task1/Program.cs
@@ -2,6 +2,7 @@
 //  целые числа с консоли. Программа завершается при вводе символа 
 //  ‘q’ или при вводе числа, сумма цифр которого чётная.
 
+/*
 int Sum(int num)    //функция для подсчета суммы цифр во введенном числе
 {
     int res = 0;
@@ -25,3 +26,32 @@ if (str == "q")
 Console.WriteLine("Пользователь вышел принудительно!");
 else
 Console.WriteLine($"Ввод окончен! Сумма цифр в числе {int.Parse(str)} равна {Sum(int.Parse(str))}");
+*/
+
+void InfEnter()
+{
+    string word;
+
+    while (true)
+    {
+        Console.Write("Output any number: ");
+        word = Console.ReadLine()!;
+
+        if (word != "q")
+        {
+            int count = 0;
+            int number = int.Parse(word);
+            for (; number > 0; number /= 10)
+                count += number % 10;
+            if (count % 2 == 0)
+            {
+                Console.WriteLine("Game is over! Number is even");
+                return;
+            }
+        }
+        else
+            return;
+    }
+}
+
+InfEnter();
