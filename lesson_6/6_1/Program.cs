b65b341fa47fffc0417ef0d5dfca1b62d62f3b47
Andrey Butusov <agbutusov@mail.ru>
2024-01-30 22:56:51 +0300

    добавил 3 задачи урока 6

---
+++ lesson_6/6_1/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Задача 1. Задайте массив символов (тип char[]). Создайте стоку
+//  из символов этого массива.
+
+char[] mass = {'a', 'b', 'c', 'd'};
+string result = "";
+foreach (char i in mass)
+{
+    result += i;
+}
+Console.WriteLine(result);
---
+++ lesson_6/6_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задача 3. Считать строку с консоли, состоящую из латинских
+//  букв в нижнем регистре. Выяснить, сколько среди введенных
+//  букв гласных.4
+
+Console.Clear();
+string str = Console.ReadLine()!;
+int FindVowel(string word)
+{
+    string vowels = "aeiou";
+    int value = 0;
+    for (int i = 0; i < word.Length; i++)
+    {
+        if (vowels.Contains(word[i]))
+            value++;
+    }
+    return value;
+}
+Console.WriteLine(FindVowel(str));
