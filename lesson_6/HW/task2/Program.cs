a1c055aecfe96d93478de9b662a8d2698a1df5dc
Andrey Butusov <agbutusov@mail.ru>
2024-02-02 23:42:47 +0300

    добавил задачу 2, немного усовершенствованную

---
+++ lesson_6/HW/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Задача 2. Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//  Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+Console.Clear();
+
+string RandomStr (int VolumeStr)        //Функция создания произвольной строки заданной длины
+{
+    string abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string str = "";
+    Random rnd = new Random();
+    for (int i= 0; i < VolumeStr; i++)
+    {
+        str += abc[rnd.Next(0, abc.Length -1)];
+    }
+    return str;
+}
+
+Console.Write("Количество символов в строке: ");
+int VolumeStr = int.Parse(Console.ReadLine()!);
+Console.Write("Сформированная строка: ");
+string str = RandomStr(VolumeStr);
+Console.WriteLine(str);
+Console.Write("Немного магии: ");
+Console.WriteLine(str.ToLower());
