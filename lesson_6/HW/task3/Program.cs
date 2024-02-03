bfbecf9cfe455dbb0907b8d01ff59095ee591a5e
Andrey Butusov <agbutusov@mail.ru>
2024-02-03 18:06:50 +0300

    отлажена работа задачи 3 в дз 6

---
+++ lesson_6/HW/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Задача 3. Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+
+using System.Text;
+
+Console.Clear();
+
+bool IsStrPalindrom(string str)
+{
+    char[] chars = str.ToCharArray();
+    
+    for (int i = 0; i < chars.Length / 2; i++)
+    {
+        if (chars[i] != chars[chars.Length - i - 1])
+            return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Введите произвольную строку");
+Console.InputEncoding = Encoding.Unicode;
+string str = Console.ReadLine()!;
+if (IsStrPalindrom(str))
+    Console.WriteLine("Введен палиндром");
+else
+    Console.WriteLine("Введен не палиндром");
