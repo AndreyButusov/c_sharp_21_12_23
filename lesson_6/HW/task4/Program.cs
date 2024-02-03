36bd99889b81eab5946e9e74cd968f06c700ef1a
Andrey Butusov <agbutusov@mail.ru>
2024-02-03 19:04:44 +0300

    добавлена задача 4 в дз 6

---
+++ lesson_6/HW/task4/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задача 4. Задайте строку, состоящую из слов, разделенных пробелами. 
+//  Сформировать строку, в которой слова расположены в обратном порядке. 
+//  В полученной строке слова должны быть также разделены пробелами.
+
+using System.Text;
+
+Console.Clear();
+
+string[] ReverseStr(string[] str)         //Функция разворота массива
+{
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        (str[i], str[str.Length - i - 1]) = (str[str.Length - i - 1], str[i]);
+    }
+    return str;
+}
+
+Console.WriteLine("Введите предложение");
+Console.InputEncoding = Encoding.Unicode;
+string InputStr = Console.ReadLine()!;
+string[] words = InputStr.Split(' ');       // создаем из заданного текста массив слов
+string[] ReverseWords = ReverseStr(words);
+Console.WriteLine($"{String.Join(" ", ReverseWords)}");
